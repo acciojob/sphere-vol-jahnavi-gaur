befa14f185077be7c411bd379e99c730668576b7
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-16 17:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault()
+	let radius = Number(document.querySelector('#radius').value.trim())
+	let volume = document.querySelector('#volume')
+
+	if(Number.isNaN(radius)){
+		volume.value = 'NaN'
+	}
+	else
+		volume.value = (4/3)*3.14*radius*radius*radius
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
